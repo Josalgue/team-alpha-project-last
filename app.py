22c00bd52f88c4d46fedc1277de4bc4cc93d7c45
Jonathan Salguero Gómez <jonathansalguero91@gmail.com>
2021-03-18 08:10:32 -0600

    Adding html page

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-# @app.route("/musicAnalysis")
-# def byCountry():
-#     return render_template("musicAnalysis.html")
+@app.route("/musicAnalysis")
+def byCountry():
+    return render_template("musicAnalysis.html")
 
 @app.route("/byCountry")
 def byCountry():
